52b7df6615f488ec73097c5760b2cc5b9a6bafed
momo <1123725815@qq.com>
2019-07-02 12:21:01 +0800

    frist commit

---
+++ user-service/src/main/java/com/momo/user/service/ServiceApplication.java
@@ -0,0 +1,15 @@
+package com.momo.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
+
+
+@EnableAutoConfiguration
+@ImportResource("provider.xml")
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class);
+    }
+
+}
